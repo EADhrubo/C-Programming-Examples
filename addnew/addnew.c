9f57e470a66cbb8eb47341505126d7fabd27eb06
eadhrubo <ehsan.ahmed.dhrubo@gmail.com>
2018-11-07 23:57:39 +0600

    Simple Addition with operator

---
+++ addnew/addnew.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+# include <conio.h>
+
+void main()
+{
+	int a, b, c;
+	printf("\n a= ");
+	scanf("%d",&a);
+	printf("\n b= ");
+	scanf("%d",&b);
+	
+	c=a+b;
+	printf("\n Addition of %d and %d is %d.",a,b,c);
+	getch();
+	
+}

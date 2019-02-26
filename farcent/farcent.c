2d2a90029bef62fdc76401d74a8e0787c263db50
eadhrubo <ehsan.ahmed.dhrubo@gmail.com>
2019-02-26 23:25:04 +0600

    Conversion between celcius and fahrenheit

---
+++ farcent/farcent.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+# include <conio.h>
+void main()
+{
+	float cent,farn;
+	printf("\n cent= ");
+	scanf(" %f",&cent);
+	farn=(cent*1.8)+32;
+	printf("\n farn= %f",farn);
+	getch();
+}

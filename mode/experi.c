b637c305627887826d11b58071aa0103d419b4fd
eadhrubo <ehsan.ahmed.dhrubo@gmail.com>
2019-05-18 14:43:44 +0600

    Calculating remainder from dividing two numbers

---
+++ mode/experi.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+# include <conio.h>
+void main()
+{
+	int x,y,z;
+	printf("\n x= ");
+	scanf(" %d",&x);
+	printf("\n y= ");
+	scanf(" %d",&y);
+	z=x%y;
+	printf("\n z=x mode y= %d.",z);
+	getch();
+}

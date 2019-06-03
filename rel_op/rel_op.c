ca985bbebfc58a339a96fccb708c8d6e603e5c49
eadhrubo <ehsan.ahmed.dhrubo@gmail.com>
2019-06-03 17:45:29 +0600

    Practising relational operation

---
+++ rel_op/rel_op.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+# include <conio.h>
+void main()
+{
+	int x,y;
+	printf("\n x= ");
+	scanf(" %d",&x);
+	printf("\n y= ");
+	scanf(" %d",&y);
+	printf("\n x<y: %d.",x<y);
+	printf("\n x<=y: %d.",x<=y);
+	printf("\n x>y: %d.",x>y);
+	printf("\n x>=y: %d.",x>=y);
+	printf("\n x==y: %d.",x==y);
+	printf("\n x!=y: %d.",x!=y);
+	getch();
+}

b4a4f9d0a0d3696a5cb098fe23ac0aaba4ba86e6
eadhrubo <ehsan.ahmed.dhrubo@gmail.com>
2019-06-03 18:03:01 +0600

    Find out size of bytes for any data type

---
+++ sizeof/sizeof.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+# include <conio.h>
+ void main () 
+ { 
+	 printf("\n\t  Integer   type data takes %d byte, \a", sizeof(int)  );
+	 printf("\n\t  Charactor type data takes %d byte, \a", sizeof(char) );
+	 printf("\n\t  Floating  type data takes %d byte, \a", sizeof(float) );
+	 printf("\n\t  Double    type data takes %d byte, \a", sizeof(double) );
+	 getch();
+ }
